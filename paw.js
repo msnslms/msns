f94918736a9fcfb70e9e196bfd331e8697440f33
msnslms <maithripalasenanayaka209@gmail.com>
2026-09-07 16:17:00 +0530

    Update paw.js

--- paw.js
+++ paw.js
@@ -4,4 +4,16 @@ if ('serviceWorker' in navigator) {
     .then(() => console.log('Service Worker registered'))
     .catch((err) => console.log('SW registration failed', err));
 }
+// Clean URL Logic inside pwa.js
+if (window.location.pathname.endsWith('.html')) {
+    const cleanPath = window.location.pathname.replace(/\.html$/, '');
+    window.history.replaceState(null, '', cleanPath + window.location.search + window.location.hash);
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("a").forEach(a => {
+        if (a.hostname === window.location.hostname && a.pathname.endsWith(".html")) {
+            a.pathname = a.pathname.replace(/\.html$/, "");
+        }
+    });
+});
